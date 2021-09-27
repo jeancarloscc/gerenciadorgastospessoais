79e39bcfe28333ba0b441f497bbff4fbeea6b73c
Jean Carlos <jeancc.costa@gmail.com>
2021-09-27 17:29:35 -0300

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Jean Carlos',
+      title: 'Flutter Jean',
       theme: ThemeData(
         // This is the theme of your application.
         //
